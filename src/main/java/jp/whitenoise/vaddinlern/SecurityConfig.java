de26f779510091f4eab75f1adb130a4ae5959b25
WhiteNoise0000 <46626065+WhiteNoise0000@users.noreply.github.com>
2023-10-19 21:26:56 +0900

    Spring Securityの非互換修正

--- src/main/java/jp/whitenoise/vaddinlern/SecurityConfig.java
+++ src/main/java/jp/whitenoise/vaddinlern/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 
@@ -32,7 +34,7 @@ public class SecurityConfig extends VaadinWebSecurity {
 	 */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().requestMatchers("/images/**");
+		web.ignoring().requestMatchers(AntPathRequestMatcher.antMatcher("/images/**"));
 		super.configure(web);
 	}
 
